53f3476610dbaa89b17dbfdf900f8aa5060a3be7
soframel <sophie@massen-ramel.net>
2025-03-31 10:16:42 +0200

    test health disabled in error

---
+++ smallrye-reactive-messaging-amqp/src/test/java/io/smallrye/reactive/messaging/amqp/HealthTest.java
@@ -0,0 +1,130 @@
+package io.smallrye.reactive.messaging.amqp;
+
+import static io.smallrye.common.constraint.Assert.assertFalse;
+import static org.awaitility.Awaitility.await;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import io.smallrye.config.SmallRyeConfigProviderResolver;
+import io.smallrye.reactive.messaging.health.HealthReport;
+import io.smallrye.reactive.messaging.providers.extension.HealthCenter;
+import io.smallrye.reactive.messaging.test.common.config.MapBasedConfig;
+
+public class HealthTest extends AmqpBrokerTestBase {
+    private WeldContainer container;
+    private final Weld weld = new Weld();
+
+    @AfterEach
+    public void cleanup() {
+        if (container != null) {
+            container.shutdown();
+        }
+
+        MapBasedConfig.cleanup();
+        SmallRyeConfigProviderResolver.instance().releaseConfig(ConfigProvider.getConfig());
+    }
+
+    @Test
+    public void testReadinessAndLivenessEnabledProducer() {
+        String addressSink = UUID.randomUUID().toString();
+
+        new MapBasedConfig()
+                .with("amqp-username", username)
+                .with("amqp-password", password)
+                .with("mp.messaging.outgoing.sink.connector", AmqpConnector.CONNECTOR_NAME)
+                .with("mp.messaging.outgoing.sink.address", addressSink)
+                .with("mp.messaging.outgoing.sink.host", host)
+                .with("mp.messaging.outgoing.sink.port", port)
+                .write();
+
+        weld.addBeanClass(ProducingBean.class);
+        container = weld.initialize();
+
+        await().until(() -> isAmqpConnectorAlive(container));
+        await().until(() -> isAmqpConnectorReady(container));
+    }
+
+    @Test
+    public void testReadinessAndLivenessEnabledConsumer() {
+        String address = UUID.randomUUID().toString();
+
+        new MapBasedConfig()
+                .with("amqp-username", username)
+                .with("amqp-password", password)
+                .with("mp.messaging.incoming.data.connector", AmqpConnector.CONNECTOR_NAME)
+                .with("mp.messaging.incoming.data.address", address)
+                .with("mp.messaging.incoming.data.host", host)
+                .with("mp.messaging.incoming.data.port", port)
+                .write();
+
+        weld.addBeanClass(ConsumptionBean.class);
+        container = weld.initialize();
+        await().until(() -> isAmqpConnectorAlive(container));
+        await().until(() -> isAmqpConnectorReady(container));
+    }
+
+    @Test
+    public void testHealthDisabledProducer() {
+        String address = UUID.randomUUID().toString();
+
+        new MapBasedConfig()
+                .with("amqp-username", username)
+                .with("amqp-password", password)
+                .with("mp.messaging.outgoing.sink.connector", AmqpConnector.CONNECTOR_NAME)
+                .with("mp.messaging.outgoing.sink.address", address)
+                .with("mp.messaging.outgoing.sink.host", host)
+                .with("mp.messaging.outgoing.sink.port", port)
+                .with("health-enabled", false)
+                .write();
+
+        weld.addBeanClass(ProducingBean.class);
+        container = weld.initialize();
+
+        await().until(() -> isAmqpConnectorAlive(container));
+        await().until(() -> isAmqpConnectorReady(container));
+
+        HealthCenter health = this.getHealthCenter();
+        List<HealthReport.ChannelInfo> livenessChannels = health.getLiveness().getChannels();
+        assertFalse(livenessChannels.stream()
+                .map(HealthReport.ChannelInfo::getChannel)
+                .anyMatch(s -> s.contains("sink")));
+    }
+
+    @Test
+    public void testHealthDisabledConsumer() {
+        String address = UUID.randomUUID().toString();
+
+        new MapBasedConfig()
+                .with("amqp-username", username)
+                .with("amqp-password", password)
+                .with("mp.messaging.incoming.data.connector", AmqpConnector.CONNECTOR_NAME)
+                .with("mp.messaging.incoming.data.address", address)
+                .with("mp.messaging.incoming.data.host", host)
+                .with("mp.messaging.incoming.data.port", port)
+                .with("health-enabled", false)
+                .write();
+
+        weld.addBeanClass(ConsumptionBean.class);
+        container = weld.initialize();
+        await().until(() -> isAmqpConnectorAlive(container));
+        await().until(() -> isAmqpConnectorReady(container));
+
+        HealthCenter health = this.getHealthCenter();
+        List<HealthReport.ChannelInfo> livenessChannels = health.getLiveness().getChannels();
+        assertFalse(livenessChannels.stream()
+                .map(HealthReport.ChannelInfo::getChannel)
+                .anyMatch(s -> s.contains("data")));
+    }
+
+    private HealthCenter getHealthCenter() {
+        return container.getBeanManager().createInstance().select(HealthCenter.class).get();
+    }
+
+}
